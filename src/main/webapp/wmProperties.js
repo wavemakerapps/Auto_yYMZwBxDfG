929ed3f73eb15dc0621207ae141aed2f23a75821
root <root@61949b3d2f95>
2017-06-28 18:12:15 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_yYMZwBxDfG",
+  "homePage" : "Main",
+  "name" : "Auto_yYMZwBxDfG",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
